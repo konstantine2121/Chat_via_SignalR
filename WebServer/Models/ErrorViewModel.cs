7153526724c88daf7bb6e30c4a1809c214ffde85
konstantine2121 <kach89kach89@gmail.com>
2025-03-17 13:55:28 +0300

    RemoveServerd

--- WebServer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebServer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
