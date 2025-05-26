eb8c5db9758c18b2f8761a561ac629b4556e6b53
Аушев Константин <echo@dev.null>
2025-05-26 16:01:01 +0300

    T_T

---
+++ Chat.AvaloniaClient/Views/Login/LoginPageView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Chat.AvaloniaClient.Views.Login;
+
+public partial class LoginPageView : UserControl
+{
+    public LoginPageView()
+    {
+        InitializeComponent();
+    }
+}
